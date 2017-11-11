ec11eb3aa55b867ecce59a3259c69dc44a3c9a5f
Jon Graca <jon.graca@libertymutual.com>
2017-11-11 14:33:41 -0500

    Updated check-in

--- src/main/java/com/lmig/gcf/rentals/controllers/RentalController.java
+++ src/main/java/com/lmig/gcf/rentals/controllers/RentalController.java
@@ -5,14 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lmig.gcf.rentals.models.Car;
+import com.lmig.gcf.rentals.models.Fleet;
 
 @Controller
 public class RentalController {
+
+	// Set a private variable for Fleet
+	private Fleet fleet;
+
+	// Constructor - setup an instance of the Fleet object
+	// So we can setup private variables that need values
+	public RentalController() {
+		fleet = new Fleet();
+	}
 
 	@RequestMapping("/")
 	public ModelAndView showMainPage() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("main");
+		mv.addObject("fleet", fleet);
 		return mv;
 	}
 
@@ -22,6 +33,11 @@ public class RentalController {
 		if (make != null && !make.isEmpty() && model != null && !model.isEmpty() && year != null && !year.isEmpty()) {
 
 			Car car = new Car(make, model, year);
+			fleet.addCar(car);
+			
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("redirect:/");
+			return mv;
 		}
 
 		ModelAndView mv = new ModelAndView();
@@ -29,10 +45,23 @@ public class RentalController {
 		return mv;
 	}
 
-	@RequestMapping("/carInfo")
-	public ModelAndView showCarInfoForm() {
+	@RequestMapping("/rentCar")
+	public ModelAndView rentOutACar(int index) {
+		
+		fleet.rentOutCar(index -1);
+		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("carInfoForm");
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+	
+	@RequestMapping("/returnCar")
+	public ModelAndView returnCar(int index) {
+		
+		fleet.makeCarAvailable(index -1);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
 		return mv;
 	}
 }
--- src/main/java/com/lmig/gcf/rentals/models/Fleet.java
+++ src/main/java/com/lmig/gcf/rentals/models/Fleet.java
@@ -4,9 +4,35 @@ import java.util.ArrayList;
 
 public class Fleet {
 
-	private ArrayList<Car> availableCars = new ArrayList<Car>();
-	
-	public void addCar(Car car) {
-		
+	private ArrayList<Car> availableCars;
+	private ArrayList<Car> unavailableCars;
+
+	public Fleet() {
+		availableCars = new ArrayList<Car>();
+		unavailableCars = new ArrayList<Car>();
 	}
+
+	public void addCar(Car car) {
+		availableCars.add(car);
+	}
+	
+	public void rentOutCar(int index) {
+		Car car = availableCars.remove(index);
+		unavailableCars.add(car);
+	}
+
+	public void makeCarAvailable(int index) {
+		Car car = unavailableCars.remove(index);
+		availableCars.add(car);
+	}
+		
+		
+	public ArrayList<Car> getAvailableCars() {
+		return availableCars;
+	}
+
+	public ArrayList<Car> getUnavailableCars() {
+		return unavailableCars;
+	}
+	
 }
