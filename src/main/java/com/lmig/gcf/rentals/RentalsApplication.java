7591b363dfe277ad8a4060be57df8c458165438e
Jon Graca <jon.graca@libertymutual.com>
2017-11-11 10:14:24 -0500

    Initial Check in of Rental App

---
+++ src/main/java/com/lmig/gcf/rentals/RentalsApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gcf.rentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalsApplication.class, args);
+	}
+}
